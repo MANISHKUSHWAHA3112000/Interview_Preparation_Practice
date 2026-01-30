5e38a1622aa928a2680d203c4884cd8cb5b6d2e8
Manish Kushwaha <kushwahamanish311@gmail.com>
2026-01-30 12:19:54 +0530

    main - second

--- JavaPractice/src/com/java/interview/coding/question/MissingAndRepeating.java
+++ JavaPractice/src/com/java/interview/coding/question/MissingAndRepeating.java
@@ -49,6 +49,7 @@ public class MissingAndRepeating {
         return result;
     }
     
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
