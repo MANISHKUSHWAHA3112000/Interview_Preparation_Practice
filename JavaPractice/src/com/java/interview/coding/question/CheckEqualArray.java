0f07c40da65335bc53aa8a61d57ac808baea248c
Manish Kushwaha <kushwahamanish311@gmail.com>
2026-02-09 11:58:06 +0530

    commit - check equal arrays

---
+++ JavaPractice/src/com/java/interview/coding/question/CheckEqualArray.java
@@ -0,0 +1,45 @@
+package com.java.interview.coding.question;
+
+import java.util.Scanner;
+
+public class CheckEqualArray {
+	
+	public static boolean isCheckEqual(int arr1[],int arr2[],int n1,int n2) {
+		
+	
+		int sum1 =0,sum2=0;
+		for(int i=0;i<n;i++) {
+			sum1+=arr1[i];
+		}
+		for(int i=0;i<n;i++) {
+			sum2+=arr2[i];
+		}
+		if(sum1==sum2) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int arr1[]= new int[n];
+		int arr2[]=new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr1[i]=sc.nextInt();
+		}
+		
+		for(int i=0;i<n;i++) {
+			arr2[i]=sc.nextInt();
+		}
+		
+		boolean ans = isCheckEqual(arr1,arr2,n);
+		
+		System.out.println("The two array are equal or not :"+ans);
+		
+		
+	}
+}
