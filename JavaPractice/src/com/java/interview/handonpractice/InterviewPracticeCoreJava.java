92d8478458e3f1ce218e54133c9f7df90c5d65fa
MANISHKUSHWAHA3112000 <kushwahamanish311@gmail.com>
2026-02-28 20:11:37 +0530

    preparation

---
+++ JavaPractice/src/com/java/interview/handonpractice/InterviewPracticeCoreJava.java
@@ -0,0 +1,28 @@
+package com.java.interview.handonpractice;
+
+public class InterviewPracticeCoreJava {
+	public static void main(String[] args) {
+		
+		/*
+     	Question:- Tell me about yourself 
+		Hello Good Morning,My name is Manish Kushwaha, I am full stack with Hand on experience in building web scale application using
+		java,spring boot,postgresql, and Angular.Currently I am working in a Capgemini as a software Engineer under the French client.
+		Taking to my technical skill, I am extremely working on the creating , handling the Restful web Api,hibernate/jpa focus on the clean
+		architecture,performance and security, on the frontend side I am working in Angular, along side with typescript,html,css forcusly work or creating 
+		the friendly and responsive user Interface , I am working on the git,github ,agile methoalogy. I am eager to learn New technology to build my technical
+		skill to increase my growth,my strength is i am passonate about learning new technology. thankyou that all from my side. */
+		
+		// What is Java ?
+		// Java is a high level Programming language developed by sun microsystem in 1995, it is widely used for building web scale application,enterpise application,
+		// android development, backend system.It is a pure object oriented programming language.
+		
+		// Is Java a platform independence, if yes then how?
+		// Yes, Java is a platform independence language, unlikely other programming language Java compiler (javac) compile the program int a bytecode(.class file)
+		// The bytecode is not tied with any other hardware, operating system. but it need the JVM to execute the program,Althrough JVM is a platform independence,byte code is associated with 
+		// any program or environment that JVM installed.
+		
+		// What is JVM?
+		// JVM stand for Java Virtual Machine,it is a java interpreter,It is responsible for converting the source code to byte code 
+	}
+
+}
