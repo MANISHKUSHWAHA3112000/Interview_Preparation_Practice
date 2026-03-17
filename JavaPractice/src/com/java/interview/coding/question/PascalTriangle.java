0f2fdc1fa482b785fff73ef64fa967fddcd5b05e
MANISHKUSHWAHA3112000 <kushwahamanish311@gmail.com>
2026-03-17 09:00:28 +0530

    changes

--- JavaPractice/src/com/java/interview/coding/question/PascalTriangle.java
+++ JavaPractice/src/com/java/interview/coding/question/PascalTriangle.java
@@ -5,8 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.*;
-import java.util.stream.*;
+;
 
 public class PascalTriangle {
 	
--- JavaPractice/src/com/java/interview/coding/question/Sort012.java
+++ JavaPractice/src/com/java/interview/coding/question/Sort012.java
@@ -5,51 +5,51 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Sort012 {
-	
-	public static List<Integer> sort0123(List<Integer> nums){
-		int low=0;
-		int mid=0;
-		int high=nums.size()-1;
-		
+
+	public static List<Integer> sort0123(List<Integer> nums) {
+		int low = 0;
+		int mid = 0;
+		int high = nums.size() - 1;
+
 		while (mid <= high) {
-            if (nums.get(mid) == 0) {
-                // swap nums[low] and nums[mid]
-                int temp = nums.get(low);
-                nums.set(low, nums.get(mid));
-                nums.set(mid, temp);
-                low++;
-                mid++;
-            } else if (nums.get(mid) == 1) {
-                mid++;
-            } else { // nums[mid] == 2
-                // swap nums[mid] and nums[high]
-                int temp = nums.get(mid);
-                nums.set(mid, nums.get(high));
-                nums.set(high, temp);
-                high--;
-            }
-        }
-        return nums;
-		
-		
+			if (nums.get(mid) == 0) {
+				
+				int temp = nums.get(low);
+				nums.set(low, nums.get(mid));
+				nums.set(mid, temp);
+				low++;
+				mid++;
+			} else if (nums.get(mid) == 1) {
+				mid++;
+			} else { // nums[mid] == 2
+				// swap nums[mid] and nums[high]
+				int temp = nums.get(mid);
+				nums.set(mid, nums.get(high));
+				nums.set(high, temp);
+				high--;
+			}
+		}
+		return nums;
+
 	}
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number of elements: ");
-        int n = sc.nextInt();
+		System.out.print("Enter number of elements: ");
+		int n = sc.nextInt();
 
-        List<Integer> list = new ArrayList<>();
-        System.out.println("Enter " + n + " elements (only 0,1,2):");
-        for (int i = 0; i < n; i++) {
-            list.add(sc.nextInt());
-        }
+		List<Integer> list = new ArrayList<>();
+		System.out.println("Enter " + n + " elements (only 0,1,2):");
+		for (int i = 0; i < n; i++) {
+			list.add(sc.nextInt());
+		}
 
-        List<Integer> ans = sort0123(list);
-        System.out.println("Sorted list: " + ans);
+		List<Integer> ans = sort0123(list);
+		System.out.println("Sorted list: " + ans);
 
-        sc.close();
+		sc.close();
 	}
 
 }
